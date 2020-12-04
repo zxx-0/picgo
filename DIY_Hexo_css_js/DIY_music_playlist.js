10ac6c83a52f96322c86b45b7165268fd5237fe2
zxx-0 <51950055+zxx-0@users.noreply.github.com>
2020-12-04 15:47:22 +0800

    Add files via upload

---
+++ DIY_Hexo_css_js/DIY_music_playlist.js
@@ -0,0 +1,75 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [
+        {
+        name: 'Feel You',
+        artist: '악의 꽃',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YUIU767YGCJ7ZBLXHAB7HSLVEVO',
+        cover: 'https://p2.music.126.net/UfY_VXNzPgGqbhddKV2vmQ==/109951165296168040.jpg?param=130y130'
+        },
+        {
+        name: 'Dream It Possible 曙光版',
+        artist: '华为',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YRLDQGKBRWRBBGZPC5JPZKXF3Y6',
+        cover: 'https://p2.music.126.net/Gfq_eVuq7lgilBf0w1g5dg==/109951163088676710.jpg?param=130y130'
+        },
+        {
+        name: '夜曲',
+        artist: '周杰伦',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YREMJSVGNNUOZEJYDDTSHHLLPWT',
+        cover: 'https://cdnmusic.migu.cn/picture/2019/1125/1028/AS001cc6d843be4301b73ae817ed07b8a4.jpg'
+        },
+        {
+        name: '给我一首歌的时间',
+        artist: '周杰伦',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YRPQX7EPLL3XZDJ7LBEIZFIMCTZ',
+        cover: 'https://cdnmusic.migu.cn/picture/2019/1031/0116/AS3f169c1d25854897bed1453a6aead54c.jpg'
+        },
+        {
+        name: '挪威的森林(Live)',
+        artist: '伍佰 & China Blue',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YWK2TBFMNTH65FZ6GTMU2NFBX7X',
+        cover: 'https://p2.music.126.net/6Lod8Ys5E6fiKZ9xrqCXug==/109951163100347037.jpg?param=130y130'  
+        },
+        {
+        name: '这一生关于你的风景',
+        artist: '隔壁老樊',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YSEY63QXVLLB5EIUID7GYTZ3CPU',
+        cover: 'https://p1.music.126.net/72pkxsrTN_zUscdzMk5mMA==/109951164289743850.jpg?param=130y130'  
+        },
+        {
+        name: '多想在平庸的生活拥抱你',
+        artist: '隔壁老樊',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YXM5XN4EMKOTZHL2B75HT3OUK6D',
+        cover: 'https://p2.music.126.net/gNbAlXamNjhR2j3aOukNhg==/109951164232796511.jpg?param=130y130'  
+        },
+        {
+        name: '只是太爱你',
+        artist: '丁芙妮',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YS7UOEAJNAWZFA2ILFOZFKWFLR7',
+        cover: 'https://p1.music.126.net/mKhOfpO72lu1vrXbXohX8A==/109951163988545723.jpg?param=130y130'  
+        },
+        {
+        name: '纸短情长',
+        artist: '烟把儿',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YQPCHQ6FCLU7FDJHQLDKETRKTE4',
+        cover: 'https://p2.music.126.net/z-4w8sRPuz64zj4KLIWWUw==/109951163056517430.jpg?param=130y130'  
+        },
+        {
+        name: '说好的幸福呢',
+        artist: '周杰伦',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YTF3S6NW4CRGRHYQWHWBJCIUHGK',
+        cover: 'https://cdnmusic.migu.cn/picture/2019/1031/0116/AS3f169c1d25854897bed1453a6aead54c.jpg'  
+        }
+        
+
+            ]
+});
+
+
+
+
+
+
+
+
