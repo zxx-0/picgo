794d0e5bc55879772e375b1685f162d8acd08cff
zxx-0 <51950055+zxx-0@users.noreply.github.com>
2021-01-11 13:50:39 +0800

    Update DIY_video_playlist.js

--- DIY_Hexo_css_js/DIY_video_playlist.js
+++ DIY_Hexo_css_js/DIY_video_playlist.js
@@ -2,5 +2,12 @@ const dp = new DPlayer({
     container: document.getElementById('dplayer'),
     video: {
         url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YUHPGTLTXNIUBH33NLBO4SHGTUM',
-           },
+    },
+    video: {
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YV763FACYR4MJDIUNRNZJZGR5FN',
+    },
+
+
+
+
 });
