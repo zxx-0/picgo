7aa5ca79ac10fbd306dcd274921344ad3ff645e9
zxx-0 <51950055+zxx-0@users.noreply.github.com>
2021-03-16 17:23:47 +0800

    Update DIY_music_playlist.js

--- DIY_Hexo_css_js/DIY_music_playlist.js
+++ DIY_Hexo_css_js/DIY_music_playlist.js
@@ -4,61 +4,61 @@ const ap = new APlayer({
         {
         name: '错位时空',
         artist: '肖恩Shaun-Gibson',
-        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YTLFKPHW5BKW5CZKTBILUYCLJU7',
+        url: 'https://onedrive.windandleaf.xyz/Website%20Data/Music/%E9%94%99%E4%BD%8D%E6%97%B6%E7%A9%BA-%E8%82%96%E6%81%A9Shaun-Gibson.m4s',
         cover: 'https://i0.hdslb.com/bfs/face/e920e434eb78359eaef1742d649f567219158382.jpg@96w_96h_1c.webp'
         },
         {
         name: '需要人陪',
         artist: '王力宏',
-        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YSFP7NSZRQFPZGZZNY5ETWIIB4X',
+        url: 'https://onedrive.windandleaf.xyz/Website%20Data/Music/%E9%9C%80%E8%A6%81%E4%BA%BA%E9%99%AA.m4a',
         cover: 'https://p1.music.126.net/Y8_pmsNld7rFuN9HTv-wOQ==/3344714371752550.jpg?param=130y130'
         },
         {
         name: '借 (Live)',
         artist: '毛不易',
-        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YXRNX726DXQHJHKGKGL3TUBPZAB',
+        url: 'https://onedrive.windandleaf.xyz/Website%20Data/Music/%E5%80%9F%20%28Live%29.mp3',
         cover: 'https://p1.music.126.net/yZN047VYz7selK4LCsi1Zg==/109951164681443868.jpg?param=130y130'
         },
         {
         name: 'Feel You',
         artist: '악의 꽃',
-        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YUIU767YGCJ7ZBLXHAB7HSLVEVO',
+        url: 'https://onedrive.windandleaf.xyz/Website%20Data/Music/Feel%20You.m4s',
         cover: 'https://p2.music.126.net/UfY_VXNzPgGqbhddKV2vmQ==/109951165296168040.jpg?param=130y130'
         },
         {
         name: 'Dream It Possible 曙光版',
         artist: '华为',
-        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YRLDQGKBRWRBBGZPC5JPZKXF3Y6',
+        url: 'https://onedrive.windandleaf.xyz/Website%20Data/Music/Dream%20It%20Possible%20%E6%9B%99%E5%85%89%E7%89%88.m4s',
         cover: 'https://p2.music.126.net/Gfq_eVuq7lgilBf0w1g5dg==/109951163088676710.jpg?param=130y130'
         },
         {
         name: '夜曲',
         artist: '周杰伦',
-        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YREMJSVGNNUOZEJYDDTSHHLLPWT',
+        url: 'https://onedrive.windandleaf.xyz/Website%20Data/Music/%E5%A4%9C%E6%9B%B2.mp3',
         cover: 'https://cdnmusic.migu.cn/picture/2019/1125/1028/AS001cc6d843be4301b73ae817ed07b8a4.jpg'
         },
         {
         name: '给我一首歌的时间',
         artist: '周杰伦',
-        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YRPQX7EPLL3XZDJ7LBEIZFIMCTZ',
+        url: 'https://onedrive.windandleaf.xyz/Website%20Data/Music/%E7%BB%99%E6%88%91%E4%B8%80%E9%A6%96%E6%AD%8C%E7%9A%84%E6%97%B6%E9%97%B4.mp3',
         cover: 'https://cdnmusic.migu.cn/picture/2019/1031/0116/AS3f169c1d25854897bed1453a6aead54c.jpg'
         },
         {
         name: '挪威的森林(Live)',
         artist: '伍佰 & China Blue',
-        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YWK2TBFMNTH65FZ6GTMU2NFBX7X',
+        url: 'https://onedrive.windandleaf.xyz/Website%20Data/Music/%E6%8C%AA%E5%A8%81%E7%9A%84%E6%A3%AE%E6%9E%97%28Live%29.m4a',
         cover: 'https://p2.music.126.net/6Lod8Ys5E6fiKZ9xrqCXug==/109951163100347037.jpg?param=130y130'  
         },
         {
         name: '只是太爱你',
         artist: '丁芙妮',
-        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YS7UOEAJNAWZFA2ILFOZFKWFLR7',
+        url: 'https://onedrive.windandleaf.xyz/Website%20Data/Music/%E5%8F%AA%E6%98%AF%E5%A4%AA%E7%88%B1%E4%BD%A0.mp3',
         cover: 'https://p1.music.126.net/mKhOfpO72lu1vrXbXohX8A==/109951163988545723.jpg?param=130y130'  
         },
         {
         name: '说好的幸福呢',
         artist: '周杰伦',
-        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YTF3S6NW4CRGRHYQWHWBJCIUHGK',
+        url: 'https://onedrive.windandleaf.xyz/Website%20Data/Music/%E8%AF%B4%E5%A5%BD%E7%9A%84%E5%B9%B8%E7%A6%8F%E5%91%A2.mp3',
         cover: 'https://cdnmusic.migu.cn/picture/2019/1031/0116/AS3f169c1d25854897bed1453a6aead54c.jpg'  
         }
         
